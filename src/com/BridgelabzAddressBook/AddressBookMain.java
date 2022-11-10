08c27427c3aa18118d797b8bea74720dfd3742a9
SurabhiSanjan <surabhisanjan@gmail.com>
2022-11-11 02:28:53 +0530

    Initialization: Ability to Create Contacts

--- src/com/BridgelabzAddressBook/AddressBookMain.java
+++ src/com/BridgelabzAddressBook/AddressBookMain.java
@@ -1,7 +1,9 @@
 package com.BridgelabzAddressBook;
 
-public class AddressBook {
+public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+        CDetails cd = new CDetails();
+        System.out.println(cd);
     }
 }
